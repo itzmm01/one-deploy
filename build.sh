648f7d1845c15fa2dd0f3121ae35252813e71e1c
p_chaooyang <p_chaooyang@tencent.com>
2021-12-16 10:04:01 +0800

    增加arm架构编译

--- build.sh
+++ build.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+
+build(){
+	go build main.go
+	mkdir -p one-backup
+	mv main one-backup/one-backup
+	if [ "$GOARCH" == "amd64" ]; then
+	    cp -rf bin one-backup/bin
+	else
+		cp -rf arm_bin one-backup/bin
+	fi
+	cp -rf example.yml one-backup/
+	chmod +x one-backup/bin/*
+	tar zcf one-backup-$GOOS-$GOARCH.tar.gz one-backup/ --remove-files
+}
+
 export CGO_ENABLED=0
 export GOOS=linux
+
+# amd64
 export GOARCH=amd64
+echo $GOOS-$GOARCH
+build
 
-go build main.go
-mkdir -p one-backup
-mv main one-backup/one-backup
-
-cp -rf bin one-backup/bin
-cp -rf example.yml one-backup/
-chmod +x one-backup/bin/*
-tar zcf one-backup-linux-amd64.tar.gz one-backup/ --remove-files
+# arm64
+export GOARCH=arm64
+echo $GOOS-$GOARCH
+build
